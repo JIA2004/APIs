1d7da506a6ee8982dcd2e40cc2228d83cad64ecd
Juani Aguirre <juani.aguirremonti@gmail.com>
2025-09-30 16:26:46 -0300

    Refactor security configuration to enhance authorization handling

--- demo/src/main/java/com/uade/tpo/demo/controllers/config/SecurityConfig.java
+++ demo/src/main/java/com/uade/tpo/demo/controllers/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.uade.tpo.demo.config;
+package com.uade.tpo.demo.controllers.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
